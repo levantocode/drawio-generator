14e320c4ea26b11825e34221da6a06dcd1574be8
levantocode <levanto.code@gmail.com>
2022-04-20 12:23:24 -0300

    Starting new Elixir Project

---
+++ lib/drawiogenerator.ex
@@ -0,0 +1,18 @@
+defmodule Drawiogenerator do
+  @moduledoc """
+  Documentation for `Drawiogenerator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Drawiogenerator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/drawiogenerator_test.exs
@@ -0,0 +1,8 @@
+defmodule DrawiogeneratorTest do
+  use ExUnit.Case
+  doctest Drawiogenerator
+
+  test "greets the world" do
+    assert Drawiogenerator.hello() == :world
+  end
+end
